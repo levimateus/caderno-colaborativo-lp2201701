3c50498be0684abb80d3909bf566feb29d2c8607
Ana <ana.lopsil@gmail.com>
2017-04-30 18:10:01 -0300

    Alterando base de dados, para suportar o post de publicacao.

--- db/cadernocolaborativo.sql
+++ db/cadernocolaborativo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05-Abr-2017 às 01:31
--- Versão do servidor: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Generation Time: 30-Abr-2017 às 23:02
+-- Versão do servidor: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -83,8 +83,7 @@ CREATE TABLE `likes_relacionamento` (
 CREATE TABLE `midia` (
   `midia_id` int(11) NOT NULL,
   `midia_tipo` int(3) NOT NULL,
-  `midia_href` varchar(50) NOT NULL,
-  `publicacao_id` int(11) NOT NULL
+  `midia_href` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -95,12 +94,14 @@ CREATE TABLE `midia` (
 
 CREATE TABLE `publicacao` (
   `publicacao_id` int(11) NOT NULL,
-  `publicacao_titulo` varchar(50) NOT NULL,
-  `publicacao_legenda` varchar(140) NOT NULL,
   `publicacao_status` tinyint(1) NOT NULL,
   `publicacao_dt` datetime NOT NULL,
   `usuario_id_autor` int(11) NOT NULL,
-  `usuario_id_professor` int(11) NOT NULL
+  `usuario_id_professor` int(11) NOT NULL,
+  `publicacao_area` varchar(50) NOT NULL,
+  `publicacao_descricao` varchar(675) NOT NULL,
+  `publicacao_tags` varchar(100) NOT NULL,
+  `midia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,8 +136,8 @@ CREATE TABLE `usuario` (
   `usuario_id` int(11) NOT NULL,
   `usuario_nome` varchar(25) NOT NULL,
   `usuario_sobrenome` varchar(50) NOT NULL,
-  `usuario_data_nasc` date NOT NULL,
-  `usuario_data_cadastro` datetime NOT NULL,
+  `usuario_data_nasc` date DEFAULT NULL,
+  `usuario_data_cadastro` datetime DEFAULT NULL,
   `usuario_prontuario` varchar(8) NOT NULL,
   `usuario_email` varchar(50) NOT NULL,
   `usuario_senha` varchar(41) NOT NULL,
@@ -144,8 +145,16 @@ CREATE TABLE `usuario` (
   `usuario_cargo` tinyint(1) NOT NULL,
   `usuario_experiencia` int(11) NOT NULL,
   `usuario_estado_acesso` tinyint(1) NOT NULL,
-  `media_id` int(11) NOT NULL
+  `media_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`usuario_id`, `usuario_nome`, `usuario_sobrenome`, `usuario_data_nasc`, `usuario_data_cadastro`, `usuario_prontuario`, `usuario_email`, `usuario_senha`, `usuario_descricao`, `usuario_cargo`, `usuario_experiencia`, `usuario_estado_acesso`, `media_id`) VALUES
+(3, 'giovani', 'fonseca', '1990-12-12', '2017-04-30 00:00:00', '123456', 'g@ifsp.edu.br', '5f4dcc3b5aa765d61d8327deb882cf99', 'oieee', 3, 0, 1, NULL),
+(4, 'password', 'password', '0000-00-00', '0000-00-00 00:00:00', '', 'password@password.com', '5f4dcc3b5aa765d61d8327deb882cf99', '1', 2, 2, 1, NULL);
 
 --
 -- Indexes for dumped tables
@@ -187,8 +196,7 @@ ALTER TABLE `likes_relacionamento`
 -- Indexes for table `midia`
 --
 ALTER TABLE `midia`
-  ADD PRIMARY KEY (`midia_id`),
-  ADD KEY `fk_publicacao_media` (`publicacao_id`);
+  ADD PRIMARY KEY (`midia_id`);
 
 --
 -- Indexes for table `publicacao`
@@ -247,17 +255,17 @@ ALTER TABLE `iftag`
 -- AUTO_INCREMENT for table `midia`
 --
 ALTER TABLE `midia`
-  MODIFY `midia_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `midia_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `publicacao`
 --
 ALTER TABLE `publicacao`
-  MODIFY `publicacao_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `publicacao_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -285,12 +293,6 @@ ALTER TABLE `likes_relacionamento`
   ADD CONSTRAINT `likes_relacionamento_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
   ADD CONSTRAINT `likes_relacionamento_ibfk_2` FOREIGN KEY (`publicacao_id`) REFERENCES `publicacao` (`publicacao_id`),
   ADD CONSTRAINT `likes_relacionamento_ibfk_3` FOREIGN KEY (`comentario_id`) REFERENCES `comentario` (`comentario_id`);
-
---
--- Limitadores para a tabela `midia`
---
-ALTER TABLE `midia`
-  ADD CONSTRAINT `midia_ibfk_1` FOREIGN KEY (`publicacao_id`) REFERENCES `publicacao` (`publicacao_id`);
 
 --
 -- Limitadores para a tabela `publicacao`
